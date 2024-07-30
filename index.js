8a99e5d363f4495852a39349b963fe2332bd411e
zakialbajili <zakyalbajili10@gmail.com>
2024-07-30 09:17:21 +0700

    fixing user in license

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ import excelController from "./controller/excelController.js"
 const app = express()
 app.use(cors())
 app.use(morgan("dev"))
-app.use(express.json({limit: '50mb'}))
-app.use(express.urlencoded({limit: '50mb',extended:true}))
+app.use(express.json({limit: '100mb'}))
+app.use(express.urlencoded({limit: '100mb',extended:true}))
 app.get("/",(req, res)=>{
     res.status(200).json({
         message:"Welcome To Training DB"
